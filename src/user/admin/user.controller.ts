452739b369a28a2a6ea83776175b3fba69476aa4
Neo <neo.lee@stagefive.com>
2025-05-18 22:30:13 +0900

    refactor: 사용자 관련 API의 오류 응답 메시지 코드 수정

--- src/user/admin/user.controller.ts
+++ src/user/admin/user.controller.ts
@@ -28,16 +28,16 @@ export class UserAdminController {
     summary: '사용자 정보 수정',
     description: '[관리자] 사용자 정보를 수정합니다.',
   })
-  @ApiBadRequestResponse({ description: '[PARAMETER_INVALID] 유효하지 않은 파라미터', type: ErrorResponse })
+  @ApiBadRequestResponse({ description: '[GATEWAY_000002] 유효하지 않은 파라미터', type: ErrorResponse })
   @ApiUnauthorizedResponse({
     description: `
-    - [INVALID_TOKEN] 토큰정보 유효하지 않음
-    - [FORBIDDEN] 접근 권한이 없음
+    - [GATEWAY_000003] 토큰정보 유효하지 않음
+    - [GATEWAY_000004] 접근 권한이 없음
     `,
     type: ErrorResponse,
   })
   @ApiNotFoundResponse({ description: '[GATEWAY_100005] 존재하지 않는 사용자', type: ErrorResponse })
-  @ApiInternalServerErrorResponse({ description: '[INTERNAL_SERVER_ERROR] 서버 오류', type: ErrorResponse })
+  @ApiInternalServerErrorResponse({ description: '[GATEWAY_000001] 서버 오류', type: ErrorResponse })
   async updateUser(@Body() body: UpdateUserAdminReq, @Param('id') id: string) {
     return plainToInstance(UpdateUserAdminRes, await this.userAdminService.updateUser({ ...body, id }));
   }
