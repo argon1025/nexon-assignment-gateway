5823b541a275d5bf6fe420d6874271c91cb729ef
Neo <neo.lee@stagefive.com>
2025-05-19 02:10:36 +0900

    feat: 이벤트 생성 및 보상 추가 시 오류 메시지 개선

--- src/event/admin/event.service.ts
+++ src/event/admin/event.service.ts
@@ -29,8 +29,10 @@ export class EventAdminService {
       return await this.eventApi.createEvent(options);
     } catch (error) {
       let errorCode = '';
+      let errorMessage = '';
       if (error instanceof EventApiException) {
         errorCode = error.getErrorCode();
+        errorMessage = error.getMessage();
       }
       switch (errorCode) {
         // 이미 존재하는 이벤트 코드
@@ -39,7 +41,7 @@ export class EventAdminService {
         }
         // 이벤트 조건 메타데이터 형식 오류
         case 'EVENT10011': {
-          throw new BadRequestException(ERROR_CODE.EVENT_CONDITION_METADATA_FORMAT_ERROR);
+          throw new BadRequestException({ ...ERROR_CODE.EVENT_CONDITION_METADATA_FORMAT_ERROR, message: errorMessage });
         }
         default: {
           Logger.error(`이벤트 생성 요청 처리 중 알 수 없는 오류발생`, error);
@@ -55,8 +57,10 @@ export class EventAdminService {
       return await this.eventApi.createReward(options);
     } catch (error) {
       let errorCode = '';
+      let errorMessage = '';
       if (error instanceof EventApiException) {
         errorCode = error.getErrorCode();
+        errorMessage = error.getMessage();
       }
       switch (errorCode) {
         // 이벤트를 찾을 수 없음
@@ -65,7 +69,10 @@ export class EventAdminService {
         }
         // 보상 추가정보 기재 오류
         case 'EVENT20006': {
-          throw new BadRequestException(ERROR_CODE.EVENT_REWARD_ADDITIONAL_INFO_FORMAT_ERROR);
+          throw new BadRequestException({
+            ...ERROR_CODE.EVENT_REWARD_ADDITIONAL_INFO_FORMAT_ERROR,
+            message: errorMessage,
+          });
         }
         default: {
           Logger.error(`이벤트 보상 추가 요청 처리 중 알 수 없는 오류발생`, error);
