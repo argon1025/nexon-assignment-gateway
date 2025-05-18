8f541f5871fd7762ff565d396060f2f4c9244039
Neo <neo.lee@stagefive.com>
2025-05-18 22:29:13 +0900

    feat: 관리자용 이벤트 생성, 보상 요청 내역 조회 추가

--- src/common/exception/error-code.ts
+++ src/common/exception/error-code.ts
@@ -4,10 +4,11 @@ import { ErrorDetail } from '../dto/error-response.dto';
  * 서비스에서 리턴할 수 있는 에러 코드를 정의
  */
 export const ERROR_CODE: Record<string, ErrorDetail> = {
-  INTERNAL_SERVER_ERROR: { name: 'INTERNAL_SERVER_ERROR', message: '서버 오류' },
-  PARAMETER_INVALID: { name: 'PARAMETER_INVALID', message: '파라미터 누락 또는 유효하지 않음' },
-  INVALID_TOKEN: { name: 'INVALID_TOKEN', message: '유효하지 않은 토큰입니다.' },
-  FORBIDDEN: { name: 'FORBIDDEN', message: '접근 권한이 없습니다.' },
+  /** 공통 에러 */
+  INTERNAL_SERVER_ERROR: { name: 'GATEWAY_000001', message: '서버 오류' },
+  PARAMETER_INVALID: { name: 'GATEWAY_000002', message: '파라미터 누락 또는 유효하지 않음' },
+  INVALID_TOKEN: { name: 'GATEWAY_000003', message: '유효하지 않은 토큰입니다.' },
+  FORBIDDEN: { name: 'GATEWAY_000004', message: '접근 권한이 없습니다.' },
 
   /** 유저 */
   USER_CREATE_FAILED: { name: 'GATEWAY_100001', message: '회원가입에 실패했습니다.' },
@@ -17,9 +18,10 @@ export const ERROR_CODE: Record<string, ErrorDetail> = {
   USER_NOT_FOUND: { name: 'GATEWAY_100005', message: '존재하지 않는 사용자입니다.' },
 
   /** 이벤트 */
-  EVENT_NOT_FOUND: { name: 'EVENT_100001', message: '이벤트를 찾을 수 없습니다.' },
-  EVENT_IS_NOT_ACTIVE: { name: 'EVENT_100002', message: '이벤트가 진행중이 아닙니다.' },
-  EVENT_CONDITION_NOT_MET: { name: 'EVENT_100003', message: '이벤트 조건을 만족하지 않습니다.' },
-  EVENT_ALREADY_REQUESTED: { name: 'EVENT_100004', message: '이미 신청한 이벤트입니다.' },
-  EVENT_NO_REWARD_AVAILABLE: { name: 'EVENT_100005', message: '신청할 수 있는 보상이 없습니다.' },
+  EVENT_NOT_FOUND: { name: 'GATEWAY_200001', message: '이벤트를 찾을 수 없습니다.' },
+  EVENT_IS_NOT_ACTIVE: { name: 'GATEWAY_200002', message: '이벤트가 진행중이 아닙니다.' },
+  EVENT_CONDITION_NOT_MET: { name: 'GATEWAY_200003', message: '이벤트 조건을 만족하지 않습니다.' },
+  EVENT_ALREADY_REQUESTED: { name: 'GATEWAY_200004', message: '이미 신청한 이벤트입니다.' },
+  EVENT_NO_REWARD_AVAILABLE: { name: 'GATEWAY_200005', message: '신청할 수 있는 보상이 없습니다.' },
+  EVENT_CODE_ALREADY_EXIST: { name: 'GATEWAY_200006', message: '이미 존재하는 이벤트 코드입니다.' },
 };
---
+++ src/event/admin/event.service.ts
@@ -0,0 +1,71 @@
+import { ConflictException, Injectable, InternalServerErrorException, Logger, NotFoundException } from '@nestjs/common';
+
+import {
+  AdminCreateEventOptions,
+  AdminCreateEventResult,
+  AdminCreateRewardOptions,
+  AdminCreateRewardResult,
+  AdminGetRewardRequestsOptions,
+  AdminGetRewardRequestsResult,
+} from './event.interface';
+import { EventApi } from '../../api/event/event.api';
+import { EventApiException } from '../../api/event/event.exception';
+import { ERROR_CODE } from '../../common/exception/error-code';
+
+@Injectable()
+export class EventAdminService {
+  constructor(private readonly eventApi: EventApi) {}
+
+  /** 이벤트 생성 */
+  async createEvent(options: AdminCreateEventOptions): Promise<AdminCreateEventResult> {
+    try {
+      return await this.eventApi.createEvent(options);
+    } catch (error) {
+      let errorCode = '';
+      if (error instanceof EventApiException) {
+        errorCode = error.getErrorCode();
+      }
+      switch (errorCode) {
+        // 이미 존재하는 이벤트 코드
+        case 'EVENT10002': {
+          throw new ConflictException(ERROR_CODE.EVENT_CODE_ALREADY_EXIST);
+        }
+        default: {
+          Logger.error(`이벤트 생성 요청 처리 중 알 수 없는 오류발생`, error);
+          throw new InternalServerErrorException(ERROR_CODE.INTERNAL_SERVER_ERROR);
+        }
+      }
+    }
+  }
+
+  /** 이벤트 보상 추가 */
+  async createReward(options: AdminCreateRewardOptions): Promise<AdminCreateRewardResult> {
+    try {
+      return await this.eventApi.createReward(options);
+    } catch (error) {
+      let errorCode = '';
+      if (error instanceof EventApiException) {
+        errorCode = error.getErrorCode();
+      }
+      switch (errorCode) {
+        // 이벤트를 찾을 수 없음
+        case 'EVENT10005': {
+          throw new NotFoundException(ERROR_CODE.EVENT_NOT_FOUND);
+        }
+        default: {
+          Logger.error(`이벤트 보상 추가 요청 처리 중 알 수 없는 오류발생`, error);
+          throw new InternalServerErrorException(ERROR_CODE.INTERNAL_SERVER_ERROR);
+        }
+      }
+    }
+  }
+
+  /** 이벤트 보상 요청 목록 조회 */
+  async getRewardRequests(options: AdminGetRewardRequestsOptions): Promise<AdminGetRewardRequestsResult> {
+    try {
+      return await this.eventApi.getAdminRewardRequests(options);
+    } catch (error) {
+      throw new InternalServerErrorException(ERROR_CODE.INTERNAL_SERVER_ERROR);
+    }
+  }
+}
